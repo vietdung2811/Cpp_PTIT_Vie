a44c1f41abb80d96dd8f47f0bcfd5c8fd858d974
k/4sht1rA <vietdung281104@gmail.com>
2023-05-15 14:15:13 +0700

    Create CPP0135.cpp

---
+++ CPP0135.cpp
@@ -0,0 +1,38 @@
+/*Cho số tự nhiên N. Nhiệm vụ của bạn là hãy liệt kê tất cả các số có đúng ba ước số. Ví dụ n=100, ta có các số 4, 9, 25, 49.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int prime[10001];
+void sieve(){
+    for (int i=0;i<=10000;i++)
+        prime[i]=1;
+    prime[0]=prime[1]=0;
+    for (int i=2;i<=10000;i++){
+        if (prime[i]){
+            for (int j=i*i;j<=10000;j+=i)
+                prime[j]=0;
+        }
+    }
+}
+
+void sol(int n){
+    int m=sqrt(n);
+    for (int i=2;i<=m;i++){
+        if (prime[i]){
+            cout<<i*i<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+int main(){
+    sieve();
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        sol(n);
+    }
+    return 0;
+}
