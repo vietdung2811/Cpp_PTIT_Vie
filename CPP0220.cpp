5578dd5b747987cb8ea8b5374f8a0cc7b48712ce
k/4sht1rA <vietdung281104@gmail.com>
2023-05-19 15:17:52 +0700

    Create CPP0220.cpp

---
+++ CPP0220.cpp
@@ -0,0 +1,24 @@
+/*Cho ma trận vuông A[N][N]. Hãy in các phần tử thuộc vùng biên.
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        int a[n][n];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>a[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0||i==n-1||j==n-1) cout<<a[i][j]<<" ";
+                else cout<<" "<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
